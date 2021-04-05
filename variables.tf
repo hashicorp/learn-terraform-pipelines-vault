7a431078f15fafe68cb8c672e2d2319e559b9e66
Robin Norwood <rnorwood@hashicorp.com>
2021-04-05 15:14:53 -0500

    Update to use EKS instead of GKE

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "consul_workspace" {
   type        = string
+  default = ""
   description = "Workspace that created the Consul cluster"
 }
 
 variable "cluster_workspace" {
   type        = string
+  default = ""
   description = "Workspace that created the Kubernetes cluster"
 }
 
 variable "organization" {
   type        = string
+  default = ""
   description = "Organization of workspace that created the Kubernetes cluster"
 }
--- vault.tf
+++ vault.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "vault" {
-  count     = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
+  count     = data.terraform_remote_state.eks_cluster.outputs.enable_consul_and_vault ? 1 : 0
   name      = "${data.terraform_remote_state.consul.outputs.release_name}-vault"
   chart     = "${path.module}/vault-helm"
   namespace = data.terraform_remote_state.consul.outputs.namespace
